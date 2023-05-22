bec436afe0723fec8b199b696dd5caf1e12638c7
Huseyin Caglayan <huseyin.caglayan@checkout.com>
2023-05-22 11:06:12 +0100

    added sln project structure

---
+++ picow-cgm-forms/Form1.cs
@@ -0,0 +1,10 @@
+namespace picow_cgm_server_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ picow-cgm-server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace picow_cgm_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
